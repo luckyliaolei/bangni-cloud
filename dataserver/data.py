7e48bcb259a65698d5ae9042bb0d9619a297e2ce
liao <lei.liao@qq.com>
2019-06-26 14:36:02 +0800

    ok

--- dataserver/data.py
+++ dataserver/data.py
@@ -6,7 +6,7 @@ import requests
 from utils import get_uuid
 
 app = Flask(__name__, static_folder='chunks')
-mate_server = 'http://127.0.0.1:5000'
+mate_server = 'http://124.16.70.103:5000'
 uuid = get_uuid()
 requests.get(mate_server + '/report', params={'uuid': uuid, 'volume': 2})
 
